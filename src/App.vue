7eccd8d74b493774785e24bff6c45794de0a1073
tomohisa07 <t.sakutomo07@gmail.com>
2024-07-31 16:34:56 +0900

    class属性のバインディング

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <script setup>
   let message = 'Hello World';
   const link = 'https://google.com';
+  const isActive = true;
+  const isBlack = true;
+
   const upperCase = () =>{
     message = message.toUpperCase();
   };
@@ -14,10 +17,28 @@
   <div v-text="message"></div>
   <div>
     <a v-bind:href="link">Google</a>
-  </div>
+    <p class='underLine' :class="{'active': isActive, 'back': isBlack}">v-bindの設定方法の確認</p>
+    <p :class="isActive ? 'active' : 'underLine'">v-bindの設定方法の確認</p>
+    <p :class="isActive">v-bindの設定方法の確認</p>
+    <p :class="[isActive, isBlack]">v-bindの設定方法の確認</p>
+    <p :class="['active', 'underLine']">v-bindの設定方法の確認</p>
+    <p :class="[{active: 'isActive'}, 'underLine']">v-bindの設定方法の確認</p>
+    <p :class="[isActive && 'active', isBlack]">v-bindの設定方法の確認</p>
+
+</div>
 </template>
 
 <style scoped>
+.active {
+  color: red;
+  font-weight: 900;
+}
+.underLine {
+  text-decoration: underline;
+}
+.back {
+  background-color: black;
+}
 .logo {
   height: 6em;
   padding: 1.5em;
